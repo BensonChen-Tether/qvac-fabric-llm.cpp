5580e06130d8b37956b5bd66299032959adda941
Mohamed Cherif <mohamed.cherif@tether.io>
2026-09-22 09:20:49 +0100

    fix duplicate definitions pre-dating this PR

--- ggml/src/ggml-cpu/quants.c
+++ ggml/src/ggml-cpu/quants.c
@@ -1097,14 +1097,6 @@ void ggml_vec_dot_tq2_0_128_q8_1_generic(
     *s = sumf;
 }
 
-void ggml_vec_dot_tq2_0_128_q8_K(int n, float * GGML_RESTRICT s, size_t bs, const void * GGML_RESTRICT vx, size_t bx, const void * GGML_RESTRICT vy, size_t by, int nrc) {
-    ggml_vec_dot_tq2_0_128_q8_K_generic(n, s, bs, vx, bx, vy, by, nrc);
-}
-
-void ggml_vec_dot_tq2_0_128_q8_1(int n, float * GGML_RESTRICT s, size_t bs, const void * GGML_RESTRICT vx, size_t bx, const void * GGML_RESTRICT vy, size_t by, int nrc) {
-    ggml_vec_dot_tq2_0_128_q8_1_generic(n, s, bs, vx, bx, vy, by, nrc);
-}
-
 void ggml_vec_dot_q2_K_q8_K_generic(int n, float * GGML_RESTRICT s, size_t bs, const void * GGML_RESTRICT vx, size_t bx, const void * GGML_RESTRICT vy, size_t by, int nrc) {
     assert(nrc == 1);
     UNUSED(nrc);
